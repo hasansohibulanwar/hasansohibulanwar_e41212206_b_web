2f9f0287bfe8bda9e80048b0ed4f6f2d948836bd
hasan sohib <aresuma307@gmail.com>
2023-02-28 19:35:12 +0700

    main

---
+++ Week1/sohibProjects/resources/views/home.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-' app()=>getLocale()) }}">
+<head>
+    <meta hcharset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Laravel</title>
+</head>
+<body>
+
+        <div class="jumbotran jumbotran-fluid">
+        <div class="container">
+            <h1 class="display-4">Halaman home </h1>
+            <p class="lead">Halaman ini merupakan Home dari portofolio website</p>
+        </div>
+        </div>
+</body>
+</html>
